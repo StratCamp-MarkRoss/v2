c4c9a797f3f2eec6969f44b43a28231ebe927771
JoshLeija <jal2303@columbia.edu>
2024-02-25 11:53:36 -0500

    Seventh Homework Submission

---
+++ JoshLeija/homework7_p1.py
@@ -0,0 +1,11 @@
+entered = []
+
+new =""
+while new != "quit":
+    new = input("Please enter a word ")
+    
+    if new != "quit":
+        entered.append(new)
+                         
+print(entered)
+
